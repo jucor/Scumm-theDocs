9bfb57d77731d304d6edcc93bad0539011dcd10b
cadihowley <cadihowley@gmail.com>
2021-03-05 15:35:17 +1300

    Adds version to sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -55,9 +55,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = "sphinx_rtd_theme"
 
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 master_doc = 'index'
 html_sidebars = {'**': ['localtoc.html']}
+html_theme_options = { 
+    'display_version': True
+}
