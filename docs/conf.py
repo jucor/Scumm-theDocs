1158a24d6fbba70f0c0fddaa728de1a110b33d55
cadihowley <cadihowley@gmail.com>
2021-03-05 16:05:28 +1300

    Changes to JS

--- docs/conf.py
+++ docs/conf.py
@@ -60,9 +60,13 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
 html_js_files=[
     'version.js'
 ]
+def setup(app):
+   app.add_js_file("version.js")
+
 master_doc = 'index'
 html_sidebars = {'**': ['localtoc.html']}
 html_theme_options = { 
